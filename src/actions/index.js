7e1a7334ccc3e768201f75c6795bf2a7cf0921cb
Eshargh Ghoodjani <e.ghoodjani@gmail.com>
2020-01-17 22:50:25 +0330

    Posts fetched!

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import jsonPlaceHolder from '../apis/jsonPlaceHolder';
+
+export const fetchPosts =  () => async dispatch => {
+            const response = await jsonPlaceHolder.get('/posts');
+            dispatch({type:'FETCH_POSTS', payload:response.data})
+};
+
+// ****** OLD SYNTAX  *******
+// export const fetchPosts =  () => {
+//     return (
+//         async function (dispatch) {
+//             const response = await jsonPlaceHolder.get('/posts');
+//             dispatch({type:'FETCH_POSTS', payload:response})
+//         }
+//     )    
+// }
